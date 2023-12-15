3f7636f2ff3ebec26d2f08ad9ea71d67bf6955ee
aimane yousr <aimaneyousr@gmail.com>
2023-12-15 01:17:29 +0100

    fixed pop_top

--- 1-stack_functions.c
+++ 1-stack_functions.c
@@ -21,3 +21,36 @@ void print_stack(stack_t **stack, unsigned int line_number)
 		temp = temp->next;
 	}
 }
+
+/**
+ * print_top_stack - Prints the top node of the stack.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: Interger representing the line number of of the opcode.
+ */
+void print_top_stack(stack_t **stack, unsigned int line_number)
+{
+	if (stack == NULL || *stack == NULL)
+		more_errors(6, line_number);
+	printf("%d\n", (*stack)->n);
+}
+
+/**
+ * pop_top_stack - Adds a node to the stack.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: Interger representing the line number of of the opcode.
+ * Return: Void.
+ */
+void pop_top_stack(stack_t **stack, unsigned int line_number)
+{
+	stack_t *next_node;
+
+	if (!stack || !*stack)
+	{
+		more_errors(7, line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	next_node = (*stack)->next;
+	free(*stack);
+	*stack = next_node;
+}
