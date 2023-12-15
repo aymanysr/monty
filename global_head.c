b3c4582ffeedcc3b52bbe05366b7df57d45cce06
aimane yousr <aimaneyousr@gmail.com>
2023-12-15 01:41:38 +0100

    definded global head

--- global_head.c
+++ global_head.c
@@ -2,12 +2,21 @@
 
 stack_t *head = NULL;
 
+/**
+ * get_global_head - returns the global head
+ * Return: the global head
+*/
 stack_t **get_global_head(void)
 {
-  return &head;
+	return (&head);
 }
 
+/**
+ * set_global_head - sets the global head
+ * @new_head: the new head
+ * Return: void
+*/
 void set_global_head(stack_t *new_head)
 {
-  head = new_head;
+	head = new_head;
 }
